4c9c531eff52a1ad13227536f106dddf7c159b74
Atticus Ezis <atticus.ezis@gmail.com>
2025-12-01 17:18:00 +0900

    added healthcheck

--- codex_test/urls.py
+++ codex_test/urls.py
@@ -16,14 +16,21 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.authentication import SessionAuthentication
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", health_check),
     path("", include("myapp.urls")),
 ]
 
